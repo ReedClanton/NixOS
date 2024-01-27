ea363bd87b659e197e58dd8446c1f5f3c1f8848c
reedclanton <clantonreed@gmail.com>
2024-01-27 12:38:07 -0600

    Minor README Correction
    Corrected example Home Manager version to the current one.

--- users/docs/home-default.nix
+++ users/docs/home-default.nix
@@ -11,7 +11,7 @@
 		# You should not change this value, even if you update Home Manager. If you do
 		# want to update the value, then make sure to first check the Home Manager
 		# release notes.
-		stateVersion = "<nixMajorVersion>.<nixMinorVersion>"; # Ex: "23.22"
+		stateVersion = "<nixMajorVersion>.<nixMinorVersion>"; # Ex: "23.11"
 		username = user.name;
 		homeDirectory = user.home;
 		shellAliases = shell.aliases;
