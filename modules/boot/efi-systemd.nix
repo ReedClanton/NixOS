1bc08a3e5c8ba39e72ce7f1ff53a96a35b3dd6ea
reedclanton <clantonreed@gmail.com>
2024-01-30 09:33:36 -0600

    Disabled systemd-boot editor
    Done to improve secrity. May need to be overwritten by some hosts for
    backwards compatibility.

--- modules/boot/efi-systemd.nix
+++ modules/boot/efi-systemd.nix
@@ -5,6 +5,8 @@
 			efi.canTouchEfiVariables = true;
 			systemd-boot = {
 				enable = true;
+				# Disabled for secrity. May need to enable for backwards compatibility.
+				editor = false;
 				configurationLimit = 10;
 			};
 			timeout = 1;
