d7e151238b3f52dfe2e62a4999f323a40ee89f73
reedclanton <clantonreed@gmail.com>
2024-01-29 21:45:48 -0600

    VM Host Fix
    Tested new VM host and found bugs. Fixed them. Also included
    hardware-configuration.nix

--- hosts/vm/modules/boot/default.nix
+++ hosts/vm/modules/boot/default.nix
@@ -1,6 +1,6 @@
 { lib, ... }: {
-	imports [
-		../../modules/boot/efi-systemd.nix
+	imports = [
+		../../../../modules/boot/efi-systemd.nix
 	];
 
 	boot.loader.systemd-boot.configurationLimit = lib.mkForce 5;
