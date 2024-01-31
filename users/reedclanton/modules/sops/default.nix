0f9cbaa8a040397b069dba93953a90bb39fec3ea
reedclanton <clantonreed@gmail.com>
2024-01-31 09:01:35 -0600

    Testing Out Sops Changes
    Testing out changes to sops-nix configuration. The hope is that I can
    place a single ssh key file in one location and not mess with keys.

--- users/reedclanton/modules/sops/default.nix
+++ users/reedclanton/modules/sops/default.nix
@@ -1,10 +1,26 @@
 { config, inputs, pkgs, sops-nix, user, ... }: {
-	environment.systemPackages = with pkgs; [ sops ];
+	environment = {
+		etc = {
+#			"keys/age/${user.name}_keys.txt" = {
+#				user = user.name;
+#				source = ../../keys.txt;
+#			};
+			"keys/ssh/ssh_host_ed25519_key_${user.name}" = {
+				user = user.name;
+				source = ../../id_ed25519;
+			};
+		};
+		systemPackages = with pkgs; [ sops ];
+	};
 
 	sops = {
 		defaultSopsFile = ../../config/sops/secrets/reedclanton.yaml;
 		defaultSopsFormat = "yaml";
-		age.keyFile = "${user.home}/.config/sops/age/keys.txt";
+		age = {
+#			keyFile = "${user.home}/.config/sops/age/keys.txt";
+#			keyFile = "/etc/keys/age/${user.name}_keys.txt";
+			sshKeyPaths = [ "/etc/keys/ssh/ssh_host_ed25519_key_${user.name}" ];
+		};
 		secrets = {
 			"app/element-desktop/message-key" = {
 				owner = config.users.users."${user.name}".name;
