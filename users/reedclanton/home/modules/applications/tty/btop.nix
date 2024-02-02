008deee2a0e478ef823adbc0d9c61da0f4d267b2
reedclanton <clantonreed@gmail.com>
2024-02-02 13:44:13 -0600

    Changed btop Configuration
    Turned off auto scalling in btop's network graph because I can't read
    the graph when auto scalling is on.

--- users/reedclanton/home/modules/applications/tty/btop.nix
+++ users/reedclanton/home/modules/applications/tty/btop.nix
@@ -2,27 +2,30 @@
 	programs.btop = {
 		enable = true;
 		settings = {
+			background_update = true;
+			clock_format = "";
 			color_theme = "kyli0x";
-			truecolor = true;
-			presets = "cpu:0:tty,net:0:tty,mem:0:tty cpu:0:tty proc:0:tty mem:0:tty net:0:tty";
-			rounded_corners = false;
-			graph_symbol = "tty";
-			shown_boxes = "cpu mem net";
-			update_ms = 500;
-			proc_sorting = "cpu lazy";
-			proc_tree = true;
-			proc_colors = true;
-			proc_gradient = true;
-			proc_per_core = false;
-			proc_mem_bytes = false;
 			cpu_graph_upper = "total";
 			cpu_graph_lower = "system";
 			cpu_invert_lower = false;
-			show_uptime = false;
-			clock_format = "";
-			background_update = false;
-			net_sync = true;
+			graph_symbol = "tty";
+			net_auto = false;
+			net_download = 20;
+			net_upload = 10;
+			net_sync = false;
+			presets = "cpu:0:tty,net:0:tty,mem:0:tty cpu:0:tty proc:0:tty mem:0:tty net:0:tty";
+			proc_colors = true;
+			proc_gradient = true;
+			proc_mem_bytes = false;
+			proc_per_core = false;
+			proc_sorting = "cpu lazy";
+			proc_tree = true;
+			rounded_corners = false;
 			show_battery = true;
+			show_uptime = false;
+			shown_boxes = "cpu mem net";
+			truecolor = true;
+			update_ms = 500;
 		};
 	};
 }
