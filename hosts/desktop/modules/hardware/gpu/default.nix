38927b506ec02ad6ed39b42facd9a2108e89ed9a
reedclanton <clantonreed@gmail.com>
2024-02-02 19:38:23 -0600

    Added Comments

--- hosts/desktop/modules/hardware/gpu/default.nix
+++ hosts/desktop/modules/hardware/gpu/default.nix
@@ -3,10 +3,12 @@
 
 	boot = {
 		initrd.kernelModules = [ "amdgpu" ];
+		# Enable support for my old ass AMD ATI R9 290X GPU.
 		kernelParams = [ "radeon.cik_support=0" "amdgpu.cik_support=1" ];
 	};
 
 	hardware.opengl.extraPackages = [ pkgs.amdvlk ];
 
+	# Enable GPU hardware acceleration for x11 (but don't turn x11 on).
 	services.xserver.videoDrivers = [ "amdgpu" ];
 }
