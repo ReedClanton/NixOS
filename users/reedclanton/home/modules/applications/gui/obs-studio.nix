3ede7fc5e540c5b38a737fe050f81779500e734f
reedclanton <clantonreed@gmail.com>
2024-02-16 11:24:05 -0600

    Added OBS Plugins

--- users/reedclanton/home/modules/applications/gui/obs-studio.nix
+++ users/reedclanton/home/modules/applications/gui/obs-studio.nix
@@ -1,12 +1,21 @@
-{ config, user, ... }: {
+{ config, pkgs, user, ... }: {
 	home.file = {
 		"${config.xdg.userDirs.videos}/OBS/.keep".text = "Home Manager can only create directories that contain something. Thus in order for Home Manager to creation the directory this file is in, a file must be created in it. Hense the existice of this file.";
 		"${config.xdg.configHome}/obs-studio/basic/profiles/${user.name}/basic.ini".source = ../../../config/obs-studio/basic.ini;
 	};
+
 	programs.obs-studio = {
 		enable = true;
-#		plugins = [
-#		];
+		plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+      input-overlay
+      obs-backgroundremoval
+      obs-gstreamer
+      obs-teleport
+      obs-tuna
+      waveform
+      wlrobs
+		];
 	};
 }
 
