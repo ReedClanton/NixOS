31d56c69afe992154c92334b2b39f6149a129604
reedclanton <clantonreed@gmail.com>
2024-02-15 19:14:05 -0600

    Added Video Streaming Utilities

--- users/reedclanton/home/hosts/framework13/default.nix
+++ users/reedclanton/home/hosts/framework13/default.nix
@@ -1,7 +1,6 @@
 { ... }: {
   imports = [
     ../default.nix
-    ./modules/stream-webcam
   ];
 }
 
--- users/reedclanton/home/modules/applications/tty/matroska.nix
+++ users/reedclanton/home/modules/applications/tty/matroska.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    ffmpeg
     libmatroska
-    v4l-utils
+    mkvtoolnix-cli
   ];
 }
 
