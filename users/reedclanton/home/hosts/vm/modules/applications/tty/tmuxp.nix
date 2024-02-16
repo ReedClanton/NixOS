9fa8e79df14e119b6af394529dc0d69963536c0f
Reed <108965432+ReedClanton@users.noreply.github.com>
2024-02-16 17:32:31 -0600

    TMUXP host specific Panel Layout (#6)
    * First pass as TMUXP Revamp
    
    Refactoring TMUXP yaml files so they are stored in ~/.config. This means
    they will work even if I move where I keep my NixOS config. Also
    updating first window panel layout of Default so it accounts for the
    screen size of each host.
    
    * TMUXP Default VM Layout Added
    
    * Refactored the Refactoring
    
    I think it is a little less confusing to have TMUXP yaml files in
    diffrent locations than it is to have some of them in nix files. Don't
    really like either way of doing it though.
    
    * Framework13 Host Layout Add & TMUXP Alias Added
    
    Added panel layout for Framework13 host. Also added an alias that
    launches all tmuxp files and attaches to the first one.

---
+++ users/reedclanton/home/hosts/vm/modules/applications/tty/tmuxp.nix
@@ -0,0 +1,8 @@
+{ config, ... }: {
+  home.file = {
+    "${config.xdg.configHome}/tmuxp/Default.yaml".source = ../../../config/tmuxp/Default.yaml;
+    "${config.xdg.configHome}/tmuxp/NixOS.yaml".source = ../../../../../config/tmuxp/NixOS.yaml;
+    "${config.xdg.configHome}/tmuxp/Web.yaml".source = ../../../../../config/tmuxp/Web.yaml;
+  };
+}
+
