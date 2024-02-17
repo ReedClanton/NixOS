293307cc980594d2533caadfa038cbe17f1c4706
reedclanton <clantonreed@gmail.com>
2024-02-16 19:21:27 -0600

    Updated VM Host to Save More Disk Space

---
+++ hosts/vm/modules/nix/default.nix
@@ -0,0 +1,12 @@
+{ lib, ... }: {
+  imports = [
+    ../../modules/nix
+  ];
+
+  ## Garbage Collection ##
+	nix.gc = {
+		dates = lib.mkForce "daily";
+		options = lib.mkForce "--delete-older-than 5d";
+	};
+}
+
