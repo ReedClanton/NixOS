333cbf587650845b2d9b843112adac98945a71ea
reedclanton <clantonreed@gmail.com>
2024-02-20 18:11:57 -0600

    Changed Gnome Terminal Font & Size

--- users/reedclanton/home/hosts/framework13/modules/gui/gnome/dconf/default.nix
+++ users/reedclanton/home/hosts/framework13/modules/gui/gnome/dconf/default.nix
@@ -1,6 +1,12 @@
-# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix (mostly)
 { lib, ... }: with lib.hm.gvariant; {
   dconf.settings = {
+    "org/gnome/Console" = {
+      custom-font = "Source Code Pro 12";
+      font-scale = 0.9;
+      use-system-font = false;
+    };
+
     "org/gnome/desktop/screensaver" = {
       lock-delay = mkUint32 180;
       lock-enabled = true;
