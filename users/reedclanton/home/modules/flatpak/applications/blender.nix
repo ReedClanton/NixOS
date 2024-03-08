f8f02e3ed8382550c28955e24af505dbe3d85294
reedclanton <clantonreed@gmail.com>
2024-03-08 15:03:09 -0600

    Removed Duplicate Copy of Blender
    blender-hip isn't needed on this machine (framework13) because it
    doesn't have a deticated GPU. It's not needed by the vm host and the
    desktop host has an AMD GPU, but it's too old to be supported.

--- users/reedclanton/home/modules/flatpak/applications/blender.nix
+++ users/reedclanton/home/modules/flatpak/applications/blender.nix
@@ -1,7 +1,4 @@
-{ pkgs, ... }: {
-	# Enable hardware accelerated rendering.
-	home.packages = [ pkgs.blender-hip ];
-
+{ ... }: {
 	services.flatpak.packages = [{ appId = "org.blender.Blender"; origin = "flathub"; }];
 }
 
