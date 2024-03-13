33ceda47d173939deb186bdfc16a869ead52a76d
reedclanton <clantonreed@gmail.com>
2024-03-13 12:51:51 -0500

    Updated VM Video Driver Name
    From what I gather, `virtualbox` is the old name and `vboxvideo` is the
    new one, so I switched.

--- hosts/vm/modules/gui/default.nix
+++ hosts/vm/modules/gui/default.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
 
   # This enables the drivers needed to support automatic screen resizing of guest display.
-  services.xserver.videoDrivers = [ "vmware" "virtualbox" "modesetting" ];
+  services.xserver.videoDrivers = [ "vmware" "vboxvideo" "modesetting" ];
 	
   # This creates a user space systemd service that runs the drivers needed to support automatic screen resizing.
   systemd.user.services = let
