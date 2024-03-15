4d75a33d616b2ac202268a5851b634377363f2e1
reedclanton <clantonreed@gmail.com>
2024-03-15 13:26:18 -0500

    Initial Secrets & KeePass Implemetnation
    Added KeePass file password to sops, added KeePass file to config,
    copied KeePass file into DDocuments directory, and added Secrets flatpak
    to GNOME folders.

--- users/reedclanton/home/modules/flatpak/applications/secrets.nix
+++ users/reedclanton/home/modules/flatpak/applications/secrets.nix
@@ -1,4 +1,7 @@
 { ... }: {
+  # Copy KeePass file into more obvious location.
+  home.file."Documents/Secrets/KeePass/reedclanton.kdbx".source = ../../../config/kee-pass/reedclanton.kdbx;
+
 	services.flatpak.packages = [{ appId = "org.gnome.World.Secrets"; origin = "flathub"; }];
 }
 
