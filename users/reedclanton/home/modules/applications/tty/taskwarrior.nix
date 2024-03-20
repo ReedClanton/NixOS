a78e2dc8b0413c0eba8bb2bed14963c7e07bc3b0
reedclanton <clantonreed@gmail.com>
2024-03-20 14:43:41 -0500

    System Update & Configured TaskWarrior
    Updated system and minor version update to kernel. Didn't update kernel
    more because v4l2loopback and virtualbox haven't been updated yet.
    Updated urgency coefficients of taskwarrior.

--- users/reedclanton/home/modules/applications/tty/taskwarrior.nix
+++ users/reedclanton/home/modules/applications/tty/taskwarrior.nix
@@ -5,6 +5,9 @@
 		config = {
 			confirmation = false;
       urgency.age.coefficient = 0.5;
+      urgency.annotations.coefficient = 0;
+      urgency.blocked.coefficient = -6;
+      urgency.blocking.coefficient = 10;
 		};
 	};
 }
