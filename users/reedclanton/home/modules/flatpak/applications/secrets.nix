c627812a106f42390b7dc16dcb633751dfd6f7d5
reedclanton <clantonreed@gmail.com>
2024-03-26 17:57:56 -0500

    Initial implementation of keepass link
    Will be changing the link to use Nix variables rather than environment
    ones. This so the same code can be used to setup users other than the
    current one.

--- users/reedclanton/home/modules/flatpak/applications/secrets.nix
+++ users/reedclanton/home/modules/flatpak/applications/secrets.nix
@@ -1,6 +1,9 @@
-{ ... }: {
+{ lib, user, ... }: {
   # Copy KeePass file into more obvious location.
-  home.file."Documents/Secrets/KeePass/reedclanton.kdbx".source = ../../../config/kee-pass/reedclanton.kdbx;
+  home.file."Documents/Secrets/KeePass/.keep".text = "Home Manager can only create directories that contain something. Thus in order for Home Manager to create the directory this file is in, a file must be created in it. Hense the existice of this file.";
+  home.activation.linkMyStuff = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ln -sf $XDG_CONFIG_HOME/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx $XDG_DOCUMENTS_DIR/Secrets/KeePass/reedclanton.txt
+  '';
 
 	services.flatpak.packages = [{ appId = "org.gnome.World.Secrets"; origin = "flathub"; }];
 }
