584fd5a85207276486bade8aa213098985dad4d7
reedclanton <clantonreed@gmail.com>
2024-03-26 18:03:03 -0500

    Switched to Nix Variables
    See last commit message for why.

--- users/reedclanton/home/modules/flatpak/applications/secrets.nix
+++ users/reedclanton/home/modules/flatpak/applications/secrets.nix
@@ -1,8 +1,8 @@
-{ lib, user, ... }: {
+{ config, lib, user, ... }: {
   # Copy KeePass file into more obvious location.
   home.file."Documents/Secrets/KeePass/.keep".text = "Home Manager can only create directories that contain something. Thus in order for Home Manager to create the directory this file is in, a file must be created in it. Hense the existice of this file.";
   home.activation.linkMyStuff = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ln -sf $XDG_CONFIG_HOME/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx $XDG_DOCUMENTS_DIR/Secrets/KeePass/reedclanton.txt
+    ln -sf ${config.xdg.configHome}/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx ${config.xdg.userDirs.documents}/Secrets/KeePass/reedclanton.txt
   '';
 
 	services.flatpak.packages = [{ appId = "org.gnome.World.Secrets"; origin = "flathub"; }];
