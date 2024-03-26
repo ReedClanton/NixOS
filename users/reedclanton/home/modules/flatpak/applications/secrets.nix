ab9ae01704c7b734e79205ae8e7d6de15581ba78
Reed <108965432+ReedClanton@users.noreply.github.com>
2024-03-26 18:32:12 -0500

    Keepass (#10)
    * Initial Secrets & KeePass Implemetnation
    
    Added KeePass file password to sops, added KeePass file to config,
    copied KeePass file into DDocuments directory, and added Secrets flatpak
    to GNOME folders.
    
    * Added to KeePass Store
    
    * Updated KeePass File
    
    * Updated KeePass File
    
    * Updated KeePass File
    
    * Updated KeePass File
    
    * Updated KeePass File
    
    * Updated KeePass
    
    * Reverted Last Change
    
    Reverted last change to kee-pass file because it broke it. Don't know
    why, but it might have been because I had it unlocked when I added it.
    
    * Initial implementation of keepass link
    
    Will be changing the link to use Nix variables rather than environment
    ones. This so the same code can be used to setup users other than the
    current one.
    
    * Switched to Nix Variables
    
    See last commit message for why.

--- users/reedclanton/home/modules/flatpak/applications/secrets.nix
+++ users/reedclanton/home/modules/flatpak/applications/secrets.nix
@@ -1,4 +1,10 @@
-{ ... }: {
+{ config, lib, user, ... }: {
+  # Copy KeePass file into more obvious location.
+  home.file."Documents/Secrets/KeePass/.keep".text = "Home Manager can only create directories that contain something. Thus in order for Home Manager to create the directory this file is in, a file must be created in it. Hense the existice of this file.";
+  home.activation.linkMyStuff = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ln -sf ${config.xdg.configHome}/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx ${config.xdg.userDirs.documents}/Secrets/KeePass/reedclanton.txt
+  '';
+
 	services.flatpak.packages = [{ appId = "org.gnome.World.Secrets"; origin = "flathub"; }];
 }
 
