0ee9717fb5ecc2ae3eef500efd22e59deb9a8fab
reedclanton <clantonreed@gmail.com>
2024-03-27 16:28:10 -0500

    Reorganized host specific HM files
    The layout now matches the rest of the home manager files.

--- users/reedclanton/home/hosts/default.nix
+++ users/reedclanton/home/hosts/default.nix
@@ -1,6 +1,8 @@
 { host, ui, ... }: {
   imports = [
-    (if builtins.pathExists ./${host}/modules/applications/tty then ./${host}/modules/applications/tty else ../../../../do-nothing.nix)
+    (if builtins.pathExists ./${host}/modules/applications/tty/config then ./${host}/modules/applications/tty/config else ../../../../do-nothing.nix)
+    (if builtins.pathExists ./${host}/modules/applications/tty/packages then ./${host}/modules/applications/tty/packages else ../../../../do-nothing.nix)
+    (if builtins.pathExists ./${host}/modules/applications/tty/programs then ./${host}/modules/applications/tty/programs else ../../../../do-nothing.nix)
     (if builtins.pathExists ./${host}/modules/gui/${ui} then ./${host}/modules/gui/${ui} else ../../../../do-nothing.nix)
     (if builtins.pathExists ./${host}/modules/hardware then ./${host}/modules/hardware else ../../../../do-nothing.nix)
     (if builtins.pathExists ./${host}/modules/networking then ./${host}/modules/networking else ../../../../do-nothing.nix)
