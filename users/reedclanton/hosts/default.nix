85af9a7c1e18f7322645e0ec82f76d267ece3281
Reed Clanton <clantonreed@gmail.com>
2024-04-01 17:53:57 +0000

    Removed Stuff to Speed Up Build

--- users/reedclanton/hosts/default.nix
+++ users/reedclanton/hosts/default.nix
@@ -1,7 +1,7 @@
 { host, ui, ... }: {
   imports = [
     (if builtins.pathExists ./${host}/modules/applications/tty then ./${host}/modules/applications/tty else ../../../../do-nothing.nix)
-    (if builtins.pathExists ./${host}/modules/gui/${ui} then ./${host}/modules/gui/${ui} else ../../../../do-nothing.nix)
+#    (if builtins.pathExists ./${host}/modules/gui/${ui} then ./${host}/modules/gui/${ui} else ../../../../do-nothing.nix)
     (if builtins.pathExists ./${host}/modules/hardware then ./${host}/modules/hardware else ../../../../do-nothing.nix)
     (if builtins.pathExists ./${host}/modules/networking then ./${host}/modules/networking else ../../../../do-nothing.nix)
   ];
