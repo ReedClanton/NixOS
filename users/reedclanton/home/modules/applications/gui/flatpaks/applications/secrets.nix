95607469efa33e0319974ecad8b952b9490f1f48
reedclanton <clantonreed@gmail.com>
2024-04-04 14:40:34 -0500

    Updated NeoVIM Spell Dictionary & KeePass Link

--- users/reedclanton/home/modules/applications/gui/flatpaks/applications/secrets.nix
+++ users/reedclanton/home/modules/applications/gui/flatpaks/applications/secrets.nix
@@ -2,7 +2,7 @@
   # Copy KeePass file into more obvious location.
   home.file."Documents/Secrets/KeePass/.keep".text = "Home Manager can only create directories that contain something. Thus in order for Home Manager to create the directory this file is in, a file must be created in it. Hense the existice of this file.";
   home.activation.linkKeePassFile = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ln -sf ${config.xdg.configHome}/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx ${config.xdg.userDirs.documents}/Secrets/KeePass/reedclanton.txt
+    ln -sf ${config.xdg.configHome}/NixOS/users/${user.name}/home/config/kee-pass/reedclanton.kdbx ${config.xdg.userDirs.documents}/Secrets/KeePass/reedclanton.kdbx
   '';
 
 	services.flatpak.packages = [{ appId = "org.gnome.World.Secrets"; origin = "flathub"; }];
