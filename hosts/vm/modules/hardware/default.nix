81e6841e60b97bf745efd801d4febc473b5580d4
reedclanton <clantonreed@gmail.com>
2024-04-09 12:07:46 -0500

    Did same for game controller as did for bluetooth

--- hosts/vm/modules/hardware/default.nix
+++ hosts/vm/modules/hardware/default.nix
@@ -2,8 +2,7 @@
   imports = [
     (if builtins.pathExists ./bluetooth/default.nix then ./bluetooth else ../../../../modules/hardware/bluetooth)
     (if builtins.pathExists ./drive/default.nix then ./drive else ../../../../do-nothing.nix)
-    # VM has no need for game controller(s).
-#    (if builtins.pathExists ./game-controller/default.nix then ./game-controller else ../../../../modules/hardware/game-controller)
+    (if builtins.pathExists ./game-controller/default.nix then ./game-controller else ../../../../modules/hardware/game-controller)
     (if builtins.pathExists ./gpu/default.nix then ./gpu else ../../../../do-nothing.nix)
     (if builtins.pathExists ./v4l2loopback/default.nix then ./v4l2loopback else ../../../../modules/hardware/v4l2loopback)
   ];
