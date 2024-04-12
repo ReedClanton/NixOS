6f41913306d5abf798c598e93f3cb88974571f4a
reedclanton <clantonreed@gmail.com>
2024-04-12 17:01:19 -0500

    Disabled wpa_supplicant for desktop host

--- hosts/desktop/modules/networking/default.nix
+++ hosts/desktop/modules/networking/default.nix
@@ -7,6 +7,10 @@
 		hostName = lib.mkForce hostName;
     # Mark internal PCI wireless interface as unmanaged so wpa_supplicant will manage it.
     networkmanager.unmanaged = [ "wlp4s6" "wlp2s0u1u1i3" ];
+    # Disable wpa_supplicant.
+    #   TODO:
+    #     - Sort out why wpa_supplicant doesn't work on this host.
+    wireless.enable = lib.mkForce false;
 	};
 }
 
