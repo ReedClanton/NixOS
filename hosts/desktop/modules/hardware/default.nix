63fc5aad973adf9b264250318b46ea246eb9e40d
reedclanton <clantonreed@gmail.com>
2024-04-15 13:34:47 -0500

    Refactored how game controller hardware is called

--- hosts/desktop/modules/hardware/default.nix
+++ hosts/desktop/modules/hardware/default.nix
@@ -2,7 +2,7 @@
   imports = [
     (if builtins.pathExists ./bluetooth/default.nix then ./bluetooth else ../../../../modules/hardware/bluetooth)
     (if builtins.pathExists ./drive/default.nix then ./drive else ../../../../do-nothing.nix)
-    (if builtins.pathExists ./game-controller/default.nix then ./game-controller else ../../../../modules/hardware/game-controller/logitech-wheel.nix)
+    (if builtins.pathExists ./game-controller/default.nix then ./game-controller else ../../../../modules/hardware/game-controller)
     (if builtins.pathExists ./gpu/default.nix then ./gpu else ../../../../do-nothing.nix)
     # TODO: Update else path to include hardware directory.
     (if builtins.pathExists ./networking/default.nix then ./networking else ../../../../modules/networking)
