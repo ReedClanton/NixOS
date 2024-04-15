a68c17cde392dc4cda23e07924ecf6ff3316c2e2
reedclanton <clantonreed@gmail.com>
2024-04-15 16:51:32 -0500

    Framework13 networking move except HM

---
+++ users/reedclanton/hosts/framework13/modules/hardware/default.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  imports = [
+    (if builtins.pathExists ./bluetooth/default.nix then ./bluetooth else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./drive/default.nix then ./drive else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./game-controller/default.nix then ./game-controller else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./gpu/default.nix then ./gpu else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./keyboard/default.nix then ./keyboard else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./networking/default.nix then ./networking else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./power/default.nix then ./power else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./printing/default.nix then ./printing else ../../../../../../do-nothing.nix)
+    (if builtins.pathExists ./v4l2loopback/default.nix then ./v4l2loopback else ../../../../../../do-nothing.nix)
+  ];
+}
+
