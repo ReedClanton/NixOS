0ce41b1494f895d513fe0b3ee0eae9f7b3e7c0e1
reedclanton <clantonreed@gmail.com>
2024-04-19 22:35:55 -0500

    Wrap up Initial desktop power Configuration

--- hosts/desktop/modules/hardware/power/tlp/default.nix
+++ hosts/desktop/modules/hardware/power/tlp/default.nix
@@ -2,5 +2,43 @@
   imports = [
     ../../../../../../modules/nixos/hardware/power/tlp
   ];
+
+  # Configure power usage.
+  services.tlp.settings = {
+    ## CPU ##
+    # AMD/Intel.
+    # TODO(s):
+    #   - Test on desktop.
+    CPU_DRIVER_OPMODE_ON_AC = "active";
+
+    # AMD/Intel.
+    # Note(s):
+    #   - Use output of `tlp-stat -p` to determine values.
+    #   - `CPU_DRIVER_OPMODE_ON_AC/BAT` must *not* be `active`.
+#    CPU_SCALING_MAX_FREQ_ON_AC = 4700000;
+#    CPU_SCALING_MIN_FREQ_ON_AC = 2550000;
+
+    ## GPU/Graphics ##
+    # AMD ATI (Legacy)
+    # TODO(s):
+    #   - Test on desktop.
+    #   - Choose desktop values.
+#    RADEON_POWER_PROFILE_ON_AC = "high";
+
+    ## Memory/RAM/Sleep ##
+    # TODO:
+    #   - Test on desktop.
+    #   - Choose desktop default.
+#    MEM_SLEEP_ON_AC = "s2idle";
+
+    ## Platform ##
+    # NoOp
+
+    ## Audio/Sound ##
+    # NoOp
+
+    ## Battery ##
+    # NoOp
+  };
 }
 
