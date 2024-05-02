e86c5e9233ec3062cd20867fc7949cc3f460f770
reedclanton <clantonreed@gmail.com>
2024-05-01 19:32:26 -0500

    Fixed Fastfetch Home Manager Install
    Was using the NixOS install configuration syntax rather than the Home
    Manager one...

--- users/reedclanton/home/modules/applications/tty/packages/fastfetch.nix
+++ users/reedclanton/home/modules/applications/tty/packages/fastfetch.nix
@@ -1,5 +1,5 @@
 { config, host, pkgs, ... }: {
-#	environment.systemPackages = with pkgs; [ fastfetch ];
+	home.packages = with pkgs; [ fastfetch ];
 	home.file."${config.xdg.configHome}/fastfetch/config.jsonc".source = (if builtins.pathExists ../../../../hosts/${host}/config/fastfetch/config.jsonc then ../../../../hosts/${host}/config/fastfetch/config.jsonc else ../../../../config/fastfetch/config.jsonc);
 }
 
