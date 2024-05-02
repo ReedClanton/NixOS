d67d449335dd19e725bfbc3e3a7a4e887790f49a
reedclanton <clantonreed@gmail.com>
2024-05-02 07:56:07 -0500

    Minor Readability Changes

--- users/reedclanton/home/modules/applications/tty/packages/fastfetch.nix
+++ users/reedclanton/home/modules/applications/tty/packages/fastfetch.nix
@@ -1,5 +1,12 @@
 { config, host, pkgs, ... }: {
-	home.packages = with pkgs; [ fastfetch ];
-	home.file."${config.xdg.configHome}/fastfetch/config.jsonc".source = (if builtins.pathExists ../../../../hosts/${host}/config/fastfetch/config.jsonc then ../../../../hosts/${host}/config/fastfetch/config.jsonc else ../../../../config/fastfetch/config.jsonc);
+	home = {
+    file."${config.xdg.configHome}/fastfetch/config.jsonc".source = (
+      if builtins.pathExists ../../../../hosts/${host}/config/fastfetch/config.jsonc then
+        ../../../../hosts/${host}/config/fastfetch/config.jsonc
+      else
+        ../../../../config/fastfetch/config.jsonc
+    );
+    packages = with pkgs; [ fastfetch ];
+  };
 }
 
