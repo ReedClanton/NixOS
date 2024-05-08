0303022ff28a28719d9e815b4c8c826ea2ce9a43
reedclanton <clantonreed@gmail.com>
2024-05-08 09:09:30 -0500

    Changed Path Of System Package List

--- modules/nixos/applications/list/default.nix
+++ modules/nixos/applications/list/default.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }: with lib; {
   # Generate list of installed package(s).
-  environment.etc."current-system-nixos-packages".text =
+  environment.etc."nixos-package-list".text =
   let
     packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
     sortedUnique = builtins.sort builtins.lessThan (unique packages);
