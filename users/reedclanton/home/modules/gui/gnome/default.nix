fd9e9ea27fa7625f63fcd46838b8525b9bad42a0
reedclanton <clantonreed@gmail.com>
2024-05-07 21:06:43 -0500

    Finished Updating Remaining Files

--- users/reedclanton/home/modules/gui/gnome/default.nix
+++ users/reedclanton/home/modules/gui/gnome/default.nix
@@ -1,6 +1,17 @@
-{ ... }: {
-	imports = [
+{ lib, user, ... }: with lib; {
+	imports =
+  let
+    # Used in log/warning/error messages.
+    current-file-path = "user/${user.name}/home/modules/gui/gnome/default.nix";
+    # Tracks location of GNOME dconf configuration.
+    dconf-configuration = "./dconf/default.nix";
+  in [
     # Configure VE.
-    (if builtins.pathExists ./dconf/default.nix then ./dconf else ../../../../../../do-nothing.nix)
+    (
+      if builtins.pathExists (./. + (builtins.substring 1 9999 "${dconf-configuration}")) then
+        ./. + (builtins.substring 1 9999 "${dconf-configuration}")
+      else
+        trivial.warn "${current-file-path}: User '${user.name}' has no GNOME dconf configuration (${dconf-configuration})." ../../../../../../do-nothing.nix
+    )
 	];
 }
