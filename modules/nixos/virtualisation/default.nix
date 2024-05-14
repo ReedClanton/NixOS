5a9202985ef7e44f1f334c5e8f9905f46720083b
reedclanton <clantonreed@gmail.com>
2024-05-14 11:12:49 -0500

    VMWare Site Down Patch
    VMWare site is down for maintenance
    (https://store-us.vmware.com/workstation_buy_dual), these changes are to
    work around this. They should be reverted when the site is back up.
    
    Also, it's nessiarily to add a file that will be used as the replacement
    package at `modules/nixos/virtualisation/vmware.bundle`. This may be
    downloaded from
    https://www.techspot.com/downloads/downloadnow/189/?evp=f14a48a23bc560f5fbe81b8d83387b41&file=10774

--- modules/nixos/virtualisation/default.nix
+++ modules/nixos/virtualisation/default.nix
@@ -1,8 +1,22 @@
-{ user, ... }: {
+{ pkgs, user, ... }: {
 	users.extraGroups.vboxuser.members = [ user.name ];
 
 	virtualisation = {
-		vmware.host.enable = true;
+		vmware.host = {
+      enable = true;
+      # Only needed while https://store-us.vmware.com/workstation_buy_dual is down for maintenance.
+      package = (pkgs.vmware-workstation.overrideAttrs rec {
+        src = ./vmware.bundle;
+        unpackPhase = let
+          vmware-unpack-env = pkgs.buildFHSEnv rec {
+            name = "vmware-unpack-env";
+            targetPkgs = pkgs: [ pkgs.zlib ];
+          };
+        in ''
+          ${vmware-unpack-env}/bin/vmware-unpack-env -c "sh ${src} --extract unpacked"
+        '';
+      });
+    };
 		virtualbox.host = {
 			enable = true;
 			enableExtensionPack = true;
