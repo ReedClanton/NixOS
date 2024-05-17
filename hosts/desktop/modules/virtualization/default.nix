0d8da29dcb6a63030d137f9008fd86e6410d4595
reedclanton <clantonreed@gmail.com>
2024-05-17 11:14:46 -0500

    Finished Initial desktop Virtualization Changes

--- hosts/desktop/modules/virtualization/default.nix
+++ hosts/desktop/modules/virtualization/default.nix
@@ -6,14 +6,19 @@
     # Tracks location of file that configures host virtualization.
     virtualization-setup = "./../../../../modules/nixos/virtualization/default.nix";
   in [
-    # Generate list of what's installed.
-    (
-      if builtins.pathExists (./. + (builtins.substring 1 9999 "${virtualization-setup}")) then
+    # Determine if host virtualization need be configured.
+    (if "${ui}" != "tty" then
+      # Call generic host virtualization configuration.
+      (if builtins.pathExists (./. + (builtins.substring 1 9999 "${virtualization-setup}")) then
         ./. + (builtins.substring 1 9999 "${virtualization-setup}")
       else
         trivial.warn
           "${current-file-path}: Common host virtualization setup not found (${virtualization-setup}), host virtualization won't be configured."
           ../../../../do-nothing.nix
+      )
+    else
+      # TTY doesn't need host virtualization setup.
+      ../../../../do-nothing.nix
     )
   ];
 }
