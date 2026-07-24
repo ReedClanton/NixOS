39bdc3923c8c2315d67d3255e6d2adcda0b88ccf
reedclanton <clantonreed@gmail.com>
2026-07-24 17:47:09 -0500

    Added fix to NixOS 25.11 for Docker
    The version of Docker on NixOS 25.11 is broken. I have to manually
    upgrade the version.

--- users/reedclanton/modules/docker/default.nix
+++ users/reedclanton/modules/docker/default.nix
@@ -1,8 +1,11 @@
-{ user, ... }: {
+{ pkgs, user, ... }: {
 	users.users.${user.name}.extraGroups = [ "docker" ];
 
 	virtualisation.docker = {
 		enable = true;
 		enableOnBoot = true;
+    # This must be done so long as I'm on version 25.11 because 25.11 is pinned to Docker 28
+    # and Docker 28 has been marked a vulnerable.
+    package = pkgs.docker_29;
 	};
 }
